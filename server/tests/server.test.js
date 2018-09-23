baa56091162fccda959428e78836e7eb5cff95ab
Lucas Czekaj <lukasz@czekaj.us>
2018-09-22 18:55:28 -0700

    Add tests for sampleClips

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,6 +5,28 @@ chai.use(require('chai-date-string'))
 const expect = chai.expect
 
 const { app, welcomeMessage, reservedUrls } = require('../server')
+const { Clip } = require('../models/clip')
+
+const sampleClips = [{
+  url: 'first',
+  text: 'This is my saved message.',
+  createdAt: new Date()
+}, {
+  url: 'second',
+  text: 'Second message',
+  createdAt: new Date()
+}, {
+  url: 'fe7kv9s8fds9f10a42da',
+  text: 'Random message',
+  createdAt: new Date()
+}
+]
+
+beforeEach((done) => {
+  Clip.deleteMany({}).then(() => {
+    return Clip.insertMany(sampleClips)
+  }).then(() => done(), (e) => { console.error(e) })
+})
 
 describe('GET /', () => {
   it('should display welcome message', (done) => {
@@ -37,6 +59,25 @@ describe('GET /favicon.ico', () => {
       .get('/favicon.ico')
       .expect(404)
       .end(done)
+  })
+})
+sampleClips.forEach((sampleClip) => {
+  describe(`GET /${sampleClip.url}`, () => {
+    it('should display an existing clip', (done) => {
+      request(app)
+        .get(`/${sampleClip.url}`).then((res) => {
+          expect(res.status).to.equal(200)
+          expect(res.body.clip.text).to.equal(sampleClip.text)
+          expect(res.body.clip.createdAt).to.be.a.dateString()
+          done()
+        }, (err) => {
+          console.error(err)
+          done(err)
+        }).catch((e) => {
+          console.error(e)
+          done(e)
+        })
+    })
   })
 })
 reservedUrls.forEach((url) => {
