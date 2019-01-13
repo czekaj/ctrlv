69c22f592081c841f3ba232013026acd2409d6f1
Lucas Czekaj <lukasz@czekaj.us>
2019-01-13 09:52:22 -0800

    New header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export default class Header extends Component {
   render = () => (
-    <div className='jumbotron jumbotron-fluid header'>
-      <h1 className='display-4 header__title'><a href='/'>CtrlV</a></h1>
-      <p className='lead'>Paste anything and visit the same url on another machine to view it</p>
+    <div className='navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar bg-primary text-white header'>
+      <div className='container header__container'>
+        <div className='header__title'>
+          <a className='navbar-brand' href='/'>ctrlv</a>
+        </div>
+        <div className='lead header__lead'>visit the same url to copy/paste anywhere</div>
+        <div className='collapse navbar-collapse'>
+          <ul className='navbar-nav'>
+            {/* <li className='nav-item lead header__lead'>Paste anything and visit the same url on another machine to view it</li> */}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,12 +4,10 @@ import Typed from 'typed.js'
 class Welcome extends Component {
   componentDidMount () {
     const options = {
-      strings: ['anything', 'you', 'can', 'easily', 'remember', 'but', 'no', 'spaces', 'or', 'special', 'characters', '...', ':)'],
+      strings: ['anything', 'you', 'can', 'easily', 'remember', 'but', 'no', 'spaces', 'or', 'special', 'characters', ':)', '...'],
       attr: 'placeholder',
       typeSpeed: 100,
-      backSpeed: 50,
-      loop: true,
-      bindInputFocusEvents: true
+      backSpeed: 50
     }
     this.typed = new Typed(this.el, options)
   }
@@ -28,7 +26,7 @@ class Welcome extends Component {
         <h5>Pick an url</h5>
         <div className='input-group input-group-lg welcome__input-group--xl'>
           <div className='input-group-prepend'>
-            <span className='input-group-text' id='new-clip-site'>https://clip.app/</span>
+            <span className='input-group-text' id='new-clip-site'>https://ctrlv.app/</span>
           </div>
           <input type='text' className='form-control' id='new-clip-key' aria-describedby='basic-addon3' placeholder='anything' ref={(el) => { this.el = el }} />
           <div className='input-group-append'>
