3d81b6c0ff29ba57e7c2e85bfa232d075d8211b5
Lucas Czekaj <lukasz@czekaj.us>
2019-01-13 10:06:22 -0800

    Add footer content

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render () {
     return (
-      <div className='footer'>
-        Footer
+      <div className='container footer'>
+        <div>
+          Made with <span role='img'>❤</span>️ in San Francisco
+        </div>
+        <div className='footer__website'>
+          <a href='https://czekaj.us'>czekaj.us</a>
+        </div>
       </div>
     )
   }
